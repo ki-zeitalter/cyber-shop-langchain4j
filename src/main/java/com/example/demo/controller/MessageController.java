2d5a75acf11bff4b55d5e8334090794f7b1e9374
sgruenheit <styrmir.gruenheit@bridging-it.de>
2025-04-06 12:15:15 +0200

    clean up

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -1,23 +1,12 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.example.demo.model.deepchat.DeepChatMessageContent;
 import com.example.demo.model.deepchat.DeepChatMessageRole;
 import com.example.demo.model.deepchat.DeepChatRequestBody;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.model.Message;
 import reactor.core.publisher.Flux;
 
 @RestController
