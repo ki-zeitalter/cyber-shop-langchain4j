f26eac55f3edac93ecba2fdfa5e5d7e3015e1b84
sgruenheit <styrmir.gruenheit@bridging-it.de>
2025-04-10 14:05:18 +0200

    Bessere Struktur

--- src/main/java/com/example/demo/assistant/ShopTools.java
+++ src/main/java/com/example/demo/assistant/ShopTools.java
@@ -1,9 +1,11 @@
 package com.example.demo.assistant;
 
+import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import com.example.demo.service.ShoppingService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.langchain4j.agent.tool.P;
 import dev.langchain4j.agent.tool.Tool;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -17,18 +19,12 @@ public class ShopTools {
     private final ObjectMapper objectMapper;
 
     @Tool("Diese Funktion gibt die Produktinformationen für das Produkt mit dem angegebenen Produktnamen zurück.")
-    public String productInformation(String productName) {
-        return productService.getProductByName(productName).map(product -> {
-            try {
-                return objectMapper.writeValueAsString(product);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        }).orElse("Produkt nicht gefunden.");
+    public Product productInformation(String productName) {
+        return productService.getProductByName(productName).orElseThrow(() -> new RuntimeException("Produkt nicht gefunden."));
     }
 
     @Tool("Diese Funktion fügt ein Produkt mit dem angegebenen Produktnamen zum Warenkorb mit der angegebenen Warenkorb-ID hinzu.")
-    public String addProductToCart(String productName, Long cartId) {
+    public String addProductToCart(@P("Der Name des Produkts") String productName, @P("Die ID des Warenkorbs")Long cartId) {
         var optionalProduct = productService.getProductByName(productName);
         if (optionalProduct.isEmpty()) {
             return "Produkt nicht gefunden.";
